22242aee432683ba1d6e66ff132be857b60d4f3e
HagerMagdy2 <hager9671@gmail.com>
2025-07-12 15:01:17 +0300

    Add project files.

---
+++ ElectronicStore.Core/Interfaces/IGenericRepositry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicStore.Core.Interfaces
+{
+    public interface IGenericRepositry<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+        Task GetByIdAsync(int Id);
+        Task GetByIdAsync(int Id, params Expression<Func<T, object>>[] includes);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ ElectronicStore.infrastructure/Repositries/GenericRepositry.cs
@@ -0,0 +1,13 @@
+﻿using ElectronicStore.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicStore.infrastructure.Repositries
+{
+    public class GenericRepositry<T>:IGenericRepositry<T> where T : class
+    {
+    }
+}
