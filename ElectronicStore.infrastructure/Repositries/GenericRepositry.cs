165e5360a2fb7075772bc96493e9532161ed6e67
HagerMagdy2 <hager9671@gmail.com>
2025-07-12 15:13:23 +0300

    initial

--- ElectronicStore.infrastructure/Repositries/GenericRepositry.cs
+++ ElectronicStore.infrastructure/Repositries/GenericRepositry.cs
@@ -2,12 +2,47 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElectronicStore.infrastructure.Repositries
 {
-    public class GenericRepositry<T>:IGenericRepositry<T> where T : class
+    public class GenericRepositry<T> : IGenericRepositry<T> where T : class
     {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByIdAsync(int Id, params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
