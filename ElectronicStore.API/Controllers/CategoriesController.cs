d4c44a1d7ea4018610dc5e496043acabab65a47b
HagerMagdy2 <hager9671@gmail.com>
2025-07-13 23:55:24 +0300

    configrations in VS

---
+++ ElectronicStore.API/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using ElectronicStore.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IUnitOfWork work;
+
+        public CategoriesController(IUnitOfWork work)
+        {
+            this.work = work;
+        }
+    }
+}
