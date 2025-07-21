94689951159dfd1b6029487b4ea7d04a334ff75a
HagerMagdy2 <hager9671@gmail.com>
2025-07-21 23:51:20 +0300

    updates

--- ElectronicStore.infrastructure/Repositries/ProductRepositry.cs
+++ ElectronicStore.infrastructure/Repositries/ProductRepositry.cs
@@ -16,16 +16,20 @@ namespace ElectronicStore.infrastructure.Repositries
     {
 
         private readonly IMapper mapper;
+        private readonly AppDbContext context;
 
         public ProductRepositry(AppDbContext context, IMapper mapper) : base(context)
         {
             this.mapper = mapper;
+            this.context = context;
         }
 
         public async Task<bool> AddAsync(AddProductDTO productDTO)
         {
             if (productDTO == null) return false;
             var product = mapper.Map<Product>(productDTO);
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
         }
     }
 }
