25a3da2316efcfce90abb5a1fb0bbda4f1898801
HagerMagdy2 <hager9671@gmail.com>
2025-07-24 22:56:34 +0300

    Error Handling

---
+++ ElectronicStore.API/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using ElectronicStore.API.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicStore.API.Controllers
+{
+    [Route("error/{StatusCode}")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Error(int StatusCode)
+        {
+           return new ObjectResult(new ResponseAPI(StatusCode));
+
+        }
+    }
+}
--- ElectronicStore.API/Program.cs
+++ ElectronicStore.API/Program.cs
@@ -24,7 +24,7 @@ namespace ElectronicStore.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
