c190e9bf5142f723db28f7a6ad666b44580ece9d
HagerMagdy2 <hager9671@gmail.com>
2025-07-25 23:29:51 +0300

    Error handling

---
+++ ElectronicStore.API/Controllers/BugController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ElectronicStore.API.Helper;
+using ElectronicStore.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicStore.API.Controllers
+{
+  
+    public class BugController : BaseController
+    {
+        public BugController(IUnitOfWork work, IMapper mapper) : base(work, mapper)
+        {
+        }
+        [HttpGet("not-found")]
+        public IActionResult GetNotFound()
+        {
+            var category = work.CategoryRepositry.GetByIdAsync(1000);
+            if(category == null)
+                return NotFound();
+            return Ok(category);
+        }
+        [HttpGet("server-error")]
+        public IActionResult ServerError()
+        {
+            var category = work.CategoryRepositry.GetByIdAsync(1000);
+            if (category == null)
+                return NotFound();
+            return Ok(category);
+        }
+    }
+}
