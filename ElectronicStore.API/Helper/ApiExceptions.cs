1e48f9a8aa3d3357ed88f4d4bfc20cd872258fce
HagerMagdy2 <hager9671@gmail.com>
2025-07-26 22:55:11 +0300

    Use MiddleWare

---
+++ ElectronicStore.API/Helper/ApiExceptions.cs
@@ -0,0 +1,11 @@
+﻿namespace ElectronicStore.API.Helper
+{
+    public class ApiExceptions : ResponseAPI
+    {
+        public ApiExceptions(int statusCode,string details=null, string? message = null) : base(statusCode, message)
+        {
+            Details = details;
+        }
+        public string Details { get; set; }
+    }
+}
--- ElectronicStore.API/Middleware/ExceptionsMiddleware.cs
+++ ElectronicStore.API/Middleware/ExceptionsMiddleware.cs
@@ -1,6 +1,33 @@
-﻿namespace ElectronicStore.API.Middleware
+﻿using ElectronicStore.API.Helper;
+using System.Net;
+using System.Text.Json;
+
+namespace ElectronicStore.API.Middleware
 {
     public class ExceptionsMiddleware
     {
+        private readonly RequestDelegate _next;
+        public ExceptionsMiddleware(RequestDelegate next)
+        {
+           _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode=(int)HttpStatusCode.InternalServerError; // Internal Server Error
+                context.Response.ContentType = "application/json";
+                var response = new ApiExceptions((int)HttpStatusCode.InternalServerError,
+                    ex.StackTrace,
+                    ex.Message);
+                var json=JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+        }
     }
+
 }
